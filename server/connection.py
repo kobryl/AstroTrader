fd7ac6b6848028585a08845c67ae499647e55987
kobryl <59873325+kobryl@users.noreply.github.com>
2023-05-11 08:36:38 +0200

    messages comments

--- server/connection.py
+++ server/connection.py
@@ -1,3 +1,79 @@
+"""
+This module handles the connection between the server and the clients.
+It is responsible for receiving messages from the clients and sending
+messages to the clients.
+
+When a client connects to the server, the server will send the game
+state and whenever the game state changes it will send an update of
+the state. The game state is a dictionary with the following keys:
+
+    - "type": "game_state" or "update"
+    - "content": {
+        - "stations": {
+            - "station": {
+                - "id": 1
+                - "data": {
+                    - "name": "PG"
+                    - "position": [0, 0]
+                    - "current_trade_modifier": 1.0
+                    - ...?
+                }
+            }
+        }
+        - "players": {
+            - "player": {
+                - "name": "Player 1"
+                - "position": [0, 0]
+                - "money": 1000
+                - "cargo": {
+                    - "name": "PG"
+                    - "amount": 0
+                }
+            }
+        }
+    }
+
+The server will receive messages from the clients with the following
+keys:
+
+    - "type": "move"
+    - "content": {
+        - "direction": [1, 0]
+    }
+
+    - "type": "buy"
+    - "content": {
+        - "item": "PG"
+        - "amount": 1000
+    }
+
+    - "type": "sell"
+    - "content": {
+        - "item": "PG"
+        - "amount": 1000
+    }
+    - "type": "mine"
+    - "content": {
+        - "item": "PG"
+        - "amount": 1000
+    }
+
+    - "type": "init"
+    - "content": {
+        - "name": "Player's name"
+    }
+
+The server may respond with the following messages:
+
+    - "type": "error"
+    - "content": {
+        - "message": "Message text"
+    }
+
+    - "type": "confirm"
+    - "content": {}
+
+"""
 import asyncio
 import json
 import queue
