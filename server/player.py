d8d3bd48615cef1e7962fcac644ed4d80c2d1c7d
Shairys <aleks.czerwionka@gmail.com>
2023-05-12 23:45:29 +0200

    lil update to logic

--- server/player.py
+++ server/player.py
@@ -2,19 +2,20 @@ from config import config
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, name):
         self.money = 0
         self.position = [0, 0]
         self.inventory = []
         self.destination = [0, 0]
         self.speed = config['default_speed']
+        self.name = name
 
-    def move(self):
+    def move(self, delta_time):
         if self.destination == self.position:
             return
         # later fix case where diagonal movement is faster than horizontal/vertical
-        self.position[0] = self.position[0] + self.speed * (self.destination[0] - self.position[0])
-        self.position[1] = self.position[1] + self.speed * (self.destination[1] - self.position[1])
+        self.position[0] = self.position[0] + self.speed * (self.destination[0] - self.position[0]) * delta_time
+        self.position[1] = self.position[1] + self.speed * (self.destination[1] - self.position[1]) * delta_time
 
     def setDestination(self, x, y):
         self.destination = [x, y]
