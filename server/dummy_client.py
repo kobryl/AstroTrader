88a8ffea1614e73a8eb5821fe67eb8bef37bb9cc
Shairys <aleks.czerwionka@gmail.com>
2023-05-13 20:34:20 +0200

    I don't know what I am doing anymore

--- server/dummy_client.py
+++ server/dummy_client.py
@@ -8,9 +8,10 @@ IP_PORT = config['server_port']
 
 def hello():
     with connect("ws://" + str(INET_ADDR) + ":" + str(IP_PORT)) as websocket:
-        websocket.send("Hello world!")
-        message = websocket.recv()
-        print(f"Received: {message}")
+        websocket.send("Joined")
+        while(True):
+            message = websocket.recv()
+            print(f"Received: {message}")
 
 if __name__ == '__main__':
     hello()
