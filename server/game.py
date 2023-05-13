e8e88564d0c18edab98d6d787c0cae22de24e60e
Shairys <aleks.czerwionka@gmail.com>
2023-05-13 19:50:39 +0200

    I don't know what I am doing anymore

--- server/game.py
+++ server/game.py
@@ -1,6 +1,11 @@
+import threading
+
 from player import Player
 from config import config
 from station import Station
+from interface import Server
+import asyncio
+
 import random
 import time
 
@@ -9,10 +14,22 @@ class Game:
     def __init__(self):
         self.state = 0
         self.players = []
-        self.net_interface = None
+        self.net_interface = Server()
+
+
+        # Define a new function that will start the server in a new thread
+        def start_server():
+            self.net_interface.start()
+
+        # Create a new thread and start the server within the new thread
+        server_thread = threading.Thread(target=start_server)
+        server_thread.start()
+
+
         self.station = Station('PG', [0, 0])
         self.delta_time = 0
         self.last_frame_time = time.time()
+        print("test:)")
 
     def start(self):
         while self.state != 2:
@@ -22,6 +39,7 @@ class Game:
             self.calculateEconomy()
             self.calculatePlayers()
             self.sendUpdates()
+
 
     def calculateEconomy(self):
         self.station.current_trade_modifier += random.random() * config['market_fluctuation']
@@ -34,6 +52,7 @@ class Game:
         pass
 
     def sendUpdates(self) -> dict[any]:
+
         update = {
                 "type": "update",
                 "content": {
