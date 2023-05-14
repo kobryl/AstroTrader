fe7e8928ab11b033dd830d03e004c20e2a339ffd
michalkr52 <46329932+michalkr52@users.noreply.github.com>
2023-05-14 19:10:25 +0200

    adjusted world size and player speed

--- server/config.py
+++ server/config.py
@@ -1,6 +1,6 @@
 
 config = {
-    'default_speed': 200,
+    'default_speed': 250,
     'default_station_trade_modifier': 1,
     'default_asteroid_mining_modifier': 5,
     'default_asteroid_mining_radius': 320,
@@ -8,8 +8,8 @@ config = {
     'server_address': '185.47.65.102',
     'server_port': 8001,
     'ticks_to_update': 60,
-    'map_size': 2048,
-    'player_start_x': 1024,
-    'player_start_y': 1024,
+    'map_size': 3072,
+    'player_start_x': 1536,
+    'player_start_y': 1536,
     'asteroid_count': 5,
 }
