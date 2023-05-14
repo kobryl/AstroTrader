3daf5e4b971d97e365e08005f8ecc93fb61a2a33
michalkr52 <46329932+michalkr52@users.noreply.github.com>
2023-05-14 20:54:06 +0200

    properly updating asteroid's properties

--- client/js/classes/Asteroid.js
+++ client/js/classes/Asteroid.js
@@ -1,16 +1,16 @@
 class Asteroid extends InteractableObject {
-    constructor(name, id, x, y, res_left, richness) {
+    constructor(name, id, x, y, resLeft, richness) {
         super(x, y, asteroidTexture, Config.ASTEROID_SCALE);
         this.name = name;
         this.id = id;
-        this.res_left = res_left;
+        this.resourcesLeft = resLeft;
         this.richness = richness;
     }
 
     update(data) {
         super.update(data);
         this.name = data.name;
-        this.res_left = data.res_left;
+        this.resourcesLeft = data.resources_left;
         this.richness = data.richness;
     }
 }
