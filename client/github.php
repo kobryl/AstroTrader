9a6b9221e7b8a5e5039924749ed76d1968d1442c
kobryl <59873325+kobryl@users.noreply.github.com>
2023-05-15 21:15:28 +0200

    Auto-pulling on raspberrypi fixed

--- client/github.php
+++ client/github.php
@@ -1,3 +1,7 @@
-<?php `git pull`;
-
+<?php 
+$result = array();
+exec("cd /home/pi/AstroTrader/ && /usr/bin/git pull 2>&1", $result);
+foreach($result as $line) {
+	print($line . "\n");
+}
 echo "done";
